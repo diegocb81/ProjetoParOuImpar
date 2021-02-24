3db700bb0bc1a6bf6911c759d6d1ba7fb1d97c9e
diegocb81 <diegocb81@gmail.com>
2021-02-23 22:02:12 -0300

    Desenvolvendo o projeto

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random meuRandom = new Random();
+        int jogadaA = meuRandom.nextInt(6);
+        int jogadaB = meuRandom.nextInt(6);
+
+    }
+
+}
